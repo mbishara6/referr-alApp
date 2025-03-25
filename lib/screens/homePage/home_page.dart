5fba75d6adc43f1deb5f8d37c268066ab8ae2e66
Bishara Muhammad Bishara <mbishara6@gmail.com>
2025-03-25 05:31:04 +0100

    referral_app

--- lib/screens/homePage/home_page.dart
+++ lib/screens/homePage/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,6 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CollectionReference profileRef   =
+  FirebaseFirestore.instance.collection("users");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,78 +31,85 @@ class _HomePageState extends State<HomePage> {
               icon: const Icon(Icons.exit_to_app))
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: CustomScrollView(
-         slivers: [
-           SliverToBoxAdapter(
-             child: Column(
-               children: [
-                 Card(
-                   child: ListTile(
-                     title:  const Text("Earning"),
-                     subtitle: Text("NGN"),
-                   ),
-                 ),
-                 const Divider(
-                   thickness: 5,
-                 ),
-                 Card(
-                   child: ListTile(
-                     title:  const Text("Referral code"),
-                     subtitle: Text("NGN"),
-                     trailing: IconButton(onPressed: (){},
-                         icon: Icon(Icons.copy_outlined),
+      body: FutureBuilder<QuerySnapshot>(
+        future: profileRef.where(
+            "refCode",
+            isEqualTo:  FirebaseAuth.instance.currentUser!.uid).get(),
+        builder: (context, snapshot) {
+          return Container(
+            padding: EdgeInsets.all(10),
+            child: CustomScrollView(
+             slivers: [
+               SliverToBoxAdapter(
+                 child: Column(
+                   children: [
+                     Card(
+                       child: ListTile(
+                         title:  const Text("Earning"),
+                         subtitle: Text("NGN"),
+                       ),
                      ),
-                   ),
-                 ),
-                 const Divider(
-                   thickness: 5,
-                 ),
-                 Card(
-                   child: Column(
-                     children: [
-                       Padding(padding: const EdgeInsets.all(60),
-                       child: const Text(
-                           "Invite your friends to earn NGN500",
-                       textAlign: TextAlign.center),
-                       ),
-                       Container(
-                         child: TextButton(onPressed: (){
-                           String sharelink = "Join our app  ";
-                         },
-                             child: const  Text("Share link"),),
-                       ),
-                       const Divider(
-                         thickness: 5,
-                       ),
-                       Container(
-                         child: Row(
-                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                           children: [
-                             Text("Referral"),
-                             Text("30")
-                           ],
+                     const Divider(
+                       thickness: 5,
+                     ),
+                     Card(
+                       child: ListTile(
+                         title:  const Text("Referral code"),
+                         subtitle: Text("NGN"),
+                         trailing: IconButton(onPressed: (){},
+                             icon: Icon(Icons.copy_outlined),
                          ),
                        ),
-                       ...List.generate(4,(index) {
-                         return Container(
-                           height: 60,
-                           margin: const EdgeInsets.only(bottom: 5),
-                           child: ListTile(
-                             leading: CircleAvatar(),
-                             title: const Text("Email address "),
+                     ),
+                     const Divider(
+                       thickness: 5,
+                     ),
+                     Card(
+                       child: Column(
+                         children: [
+                           Padding(padding: const EdgeInsets.all(60),
+                           child: const Text(
+                               "Invite your friends to earn NGN500",
+                           textAlign: TextAlign.center),
                            ),
-                         );
-                       } ,),
-                     ],
-                   ),
-                 )
-               ],
-             ),
-           )
-         ],
-        ),
+                           Container(
+                             child: TextButton(onPressed: (){
+                               String sharelink = "Join our app  ";
+                             },
+                                 child: const  Text("Share link"),),
+                           ),
+                           const Divider(
+                             thickness: 5,
+                           ),
+                           Container(
+                             child: Row(
+                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                               children: [
+                                 Text("Referral"),
+                                 Text("30")
+                               ],
+                             ),
+                           ),
+                           ...List.generate(4,(index) {
+                             return Container(
+                               height: 60,
+                               margin: const EdgeInsets.only(bottom: 5),
+                               child: ListTile(
+                                 leading: CircleAvatar(),
+                                 title: const Text("Email address "),
+                               ),
+                             );
+                           } ,),
+                         ],
+                       ),
+                     )
+                   ],
+                 ),
+               )
+             ],
+            ),
+          );
+        },
       ) ,
     );
   }
